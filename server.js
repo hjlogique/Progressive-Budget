0348bd05a0de00e403fec40c5ec6765bf52ee02a
Henry Logique <hjlogique@yahoo.com>
2020-12-18 21:46:53 -0500

    deploying

--- server.js
+++ server.js
@@ -18,6 +18,8 @@ app.use(express.static("public"));
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
